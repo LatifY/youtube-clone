80f8c5d486eb64b944ee43c292245ce3d7c41d69
Abdullatif YILMAZ <latifyilmaza@gmail.com>
2021-04-13 22:41:33 +0300

    youtube clone

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, url_for, render_template, request, redirect, session
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "key"
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return (render_template('404.html'), 404)
